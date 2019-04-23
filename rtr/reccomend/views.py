819f576a9d8d0d4de3e1ec48269713f2ef790bac
marcoortiztorres <maor9361@colorado.edu>
2019-04-23 16:26:38 -0600

    temp page views

--- rtr/reccomend/views.py
+++ rtr/reccomend/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def tailorTag(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def explore(request):
+    response = "You're looking at reccomendations based on just your sizes"
+    return HttpResponse(response)
+
+def event(request, event_type):
+    return HttpResponse("You're looking at reccomendations for %s." % event_type)
