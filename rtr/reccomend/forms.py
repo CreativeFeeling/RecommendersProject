0a04b9ce76c96687b843ad6f7116c574e28f6462
marcoortiztorres <maor9361@colorado.edu>
2019-05-01 10:58:51 -0600

    added dependencies

--- rtr/reccomend/forms.py
+++ rtr/reccomend/forms.py
@@ -3,6 +3,53 @@ from django import forms
 class TailorForm(forms.Form):
     Age = forms.IntegerField(label='age')
     Size = forms.IntegerField(label='size')
-    Bust = forms.IntegerField(label='bust')
-    Clothing_Type = forms.ChoiceField(label='clothingt',choices=[("dress","dress")])
-    Occasion = forms.ChoiceField(label='occasion',choices=[("N/A", "nothing specific"),("wedding","wedding"),("date","data"),("weekend","weekend")])
+    Bust = forms.ChoiceField(label='bust',
+                            choices=[
+                                ('A','A30'),
+                                ('A','A32'),
+                                ('A','A34'),
+                                ('A','A36'),
+                                ('A','A38'),
+                                ('A','A40'),
+                                ('B','B30'),
+                                ('B','B32'),
+                                ('B','B34'),
+                                ('B','B36'),
+                                ('B','B38'),
+                                ('B','B40'),
+                                ('C','C30'),
+                                ('C','C32'),
+                                ('C','C34'),
+                                ('C','C36'),
+                                ('C','C38'),
+                                ('C','C40'),
+                                ('D','D30'),
+                                ('D','D32'),
+                                ('D','D34'),
+                                ('D','D36'),
+                                ('D','D38'),
+                                ('D','D40'),
+                                ('DD','DD30'),
+                                ('DD','DD32'),
+                                ('DD','DD34'),
+                                ('DD','DD36'),
+                                ('DD','DD38'),
+                                ('DD','DD40'),
+                                ('DDD','DDD30'),
+                                ('DDD','DDD32'),
+                                ('DDD','DDD34'),
+                                ('DDD','DDD36'),
+                                ('DDD','DDD38'),
+                                ('DDD','DDD40'),
+                            ])
+    Clothing_Type = forms.ChoiceField(label='clothingt',
+                            choices=[
+                                ("dress","dress")
+                            ])
+    Occasion = forms.ChoiceField(label='occasion',
+                            choices=[
+                                ("N/A", "nothing specific"),
+                                ("wedding","wedding"),
+                                ("date","data"),
+                                ("weekend","weekend")
+                            ])
--- rtr/reccomend/models.py
+++ rtr/reccomend/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 class ClothingItem(models.Model):
-    item_id = models.IntegerField(blank=True, null=True)
+    item_id = models.IntegerField(blank=True,primary_key=True)
     event_type = models.CharField(max_length=50)
     avg_rating = models.IntegerField(blank=True, null=True)
     img_url = models.CharField(max_length=200)
 
+class User(models.Model):
+    user_id = models.IntegerField(blank=True, primary_key=True)
+    colthing_bust = models.CharField(max_length=5)
+    colthing_size = models.CharField(max_length=50)
+    body_shape = models.CharField(max_length=50)
+
 class Rating(models.Model):
-    item_id = models.IntegerField(blank=True, null=True)
-    user_id = models.IntegerField(blank=True, null=True)
+    item_id = models.ForeignKey(ClothingItem, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     colthing_bust = models.IntegerField(blank=True, null=True)
     colthing_size = models.CharField(max_length=50)
     body_shape = models.CharField(max_length=50)
     rating_val = models.IntegerField(blank=True, null=True)
     rating_text = models.CharField(max_length=200)
-
-class User(models.Model):
-    user_id = models.IntegerField(blank=True, null=True)
-    colthing_bust = models.IntegerField(blank=True, null=True)
-    colthing_size = models.CharField(max_length=50)
-    body_shape = models.CharField(max_length=50)
 
 class Event(models.Model):
     event_name = models.CharField(max_length=50)
